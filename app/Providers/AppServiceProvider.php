43ec9a7338ee8af805241f85556bb869c048f6a7
JLuisApaza <62665524+japazamachicao@users.noreply.github.com>
2026-01-12 04:41:07 -0500

    Register GCS driver manually in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,22 @@ class AppServiceProvider extends ServiceProvider
             \Illuminate\Support\Facades\URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS', 'on');
         }
+
+        // Register Google Cloud Storage driver
+        \Illuminate\Support\Facades\Storage::extend('gcs', function ($app, $config) {
+            $client = new \Google\Cloud\Storage\StorageClient([
+                'projectId' => $config['project_id'] ?? env('GOOGLE_CLOUD_PROJECT_ID'),
+                'keyFile' => $config['key_file'] ?? null,
+            ]);
+
+            $bucket = $client->bucket($config['bucket']);
+            $adapter = new \League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter($bucket, $config['path_prefix'] ?? '');
+            
+            return new \Illuminate\Filesystem\FilesystemAdapter(
+                new \League\Flysystem\Filesystem($adapter, $config),
+                $adapter,
+                $config
+            );
+        });
     }
 }
