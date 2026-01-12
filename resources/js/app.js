33e40024a0830e64f3cef5100d7c2e195e22b25a
JLuisApaza <62665524+japazamachicao@users.noreply.github.com>
2026-01-12 03:15:20 -0500

    Fix: Add Alpine.js Collapse plugin for x-collapse directive

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+
+// Register Alpine plugins
+Alpine.plugin(collapse);
 
 window.Alpine = Alpine;
 
