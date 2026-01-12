edc57e4b6469ddf30a65ac059a7d2e9eceace198
JLuisApaza <62665524+japazamachicao@users.noreply.github.com>
2026-01-12 09:02:24 -0500

    Add temporaryUrl support to GCS adapter for Livewire previews

--- app/Support/GoogleCloudStorageAdapter.php
+++ app/Support/GoogleCloudStorageAdapter.php
@@ -28,4 +28,19 @@ class GoogleCloudStorageAdapter extends FilesystemAdapter
             ltrim($path, '/')
         );
     }
+
+    /**
+     * Get a temporary URL for the file at the given path.
+     *
+     * @param  string  $path
+     * @param  \DateTimeInterface  $expiration
+     * @param  array  $options
+     * @return string
+     */
+    public function temporaryUrl($path, $expiration, array $options = [])
+    {
+        // For public buckets, just return the regular URL
+        // Livewire uses this for preview images
+        return $this->url($path);
+    }
 }
